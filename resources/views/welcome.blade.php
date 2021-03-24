8fcfb7b0611231c983303e46121e03fe02db5dab
wisnu518 <wisnuwisnu466@gmail.com>
2021-03-24 13:23:11 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Tugas | Laravel 8</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css') }}">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="l"><b>My</b>Profile</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <div class="card-body ">
+        <div class="row">
+            <div class="col-md-12 offset-md-6">
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <th style="width: 30%">NIM</th>
+                            <td>: 18090081/td>
+                        </tr>
+                        <tr>
+                            <th>Nama</th>
+                            <td>: Wisnu Kusumo Jati</td>
+                        </tr>
+                        <tr>
+                            <th>Kelas</th>
+                            <td>: 6B</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!-- /.social-auth-links -->
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 2.2.3 -->
+<script src="{{ asset('plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+<!-- iCheck -->
+<script src="{{ asset('plugins/iCheck/icheck.min.js') }}"></script>
+</body>
+</html>
+
